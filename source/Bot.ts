9d11382c28d9dc03b6cc51478806754de0431c48
Bolaside <bolaside@gmail.com>
2024-06-01 15:02:01 +0300

    automated event listening cause why commands are and events aren't

--- source/Bot.ts
+++ source/Bot.ts
@@ -1,8 +1,8 @@
 import { Client, GatewayIntentBits } from "discord.js"
+import { readdirSync } from "node:fs"
 import * as dotenv from "dotenv"
-
-import ready from "./events/ready"
-import messageCreate from "./events/messageCreate"
+import * as path from "path"
+import Logger from "./Logger"
 
 dotenv.config()
 
@@ -19,7 +19,28 @@ const client = new Client({
   }
 })
 
-ready(client)
-messageCreate(client)
+// Copied from thje command handler lolll
+const setupEvents = async (): Promise<number> => {
+  const eventFolder = path.join(__dirname, "events")
+  const eventFiles = readdirSync(eventFolder).filter((fileName) => fileName.endsWith(".ts"))
+  let eventCount = 0
+
+  eventFiles.forEach(async (file) => {
+    const filePath = path.join(eventFolder, file)
+
+    await import(filePath)
+      .then((event) => {
+        event.default(client)
+        eventCount++
+      })
+      .catch(Logger.error)
+  })
+
+  return eventCount
+}
+
+await setupEvents()
+  .then((eventCount) => Logger.info(`Listening to ${eventCount} events!`))
+  .catch(Logger.error)
 
 client.login(process.env.DISCORD_TOKEN)
