32eeaae548e51b5c62ca8439896da674a9428937
Bolaside <bolaside@gmail.com>
2024-06-01 15:05:20 +0300

    thjis is a class now, a new world of possibilities (possibilities in question: shorter code)

--- source/modules/CommandMessage.ts
+++ source/modules/CommandMessage.ts
@@ -1,13 +1,46 @@
-import { Message } from "discord.js"
+import { Message, MessagePayload } from "discord.js"
+import type { MessageReplyOptions } from "discord.js"
+
+import type { Embed } from "./Embed"
 
 export type FlagValue = string | number | boolean
 export type Flags = Record<string, FlagValue>
 
-export interface CommandMessage {
+export const DEFAULT_FLAG_VALUE = true
+
+interface CommandMessageObject {
   name: string
   args: Array<string>
   flags: Flags
   message: Message
 }
 
-export const DefaultFlagValue = true
+export default class CommandMessage {
+  public name: string
+  public args: Array<string>
+  public flags: Flags
+  public message: Message
+
+  private constructor({ name, args, flags, message }: CommandMessageObject) {
+    this.name = name
+    this.args = args
+    this.flags = flags
+    this.message = message
+  }
+
+  static create(commandMessage: CommandMessageObject) {
+    return new CommandMessage(commandMessage)
+  }
+
+  public hasFlag(name: string): boolean {
+    return Object.hasOwn(this.flags, name)
+  }
+
+  public reply(options: string | MessagePayload | MessageReplyOptions): Promise<Message<boolean>> {
+    return this.message.reply(options)
+  }
+
+  public replyWithEmbed(embed: Embed | Array<Embed>): Promise<Message<boolean>> {
+    return this.reply({ embeds: Array.isArray(embed) ? embed : [embed] })
+  }
+}
